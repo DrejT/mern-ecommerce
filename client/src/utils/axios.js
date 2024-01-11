8620b7f696bd3e7068d7468f1009ad4142a65051
DrejT <vivekpatel4049@gmail.com>
2024-01-12 04:28:19 +0530

    update

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -4,6 +4,7 @@ import { API_URL } from "../constants";
 const ax = axios.create({
   baseURL: API_URL,
   withCredentials: true,
+  headers: { "Access-Control-Allow-Origin": API_URL },
 });
 
 export default ax;
