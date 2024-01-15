d8918fd4accfa01e10c98b981580a0c5cee931bb
DrejT <vivekpatel4049@gmail.com>
2024-01-15 09:09:29 +0530

    update

--- server/src/db/redis.connect.js
+++ server/src/db/redis.connect.js
@@ -1,18 +1,15 @@
 require("dotenv").config();
-const redis = require("redis");
+const Redis = require("ioredis");
 
-const client = redis.createClient({
-  url: `rediss://default:${process.env.REDIS_PASSWORD}@eu2-tops-quagga-31391.upstash.io:31391`,
-});
+const client = new Redis(
+  `rediss://default:${process.env.REDIS_PASSWORD}@eu2-tops-quagga-31391.upstash.io:31391`
+);
 
-(async () => {
-  await client
-    .on("connect", () => console.log("redis connected"))
-    .on("ready", () => console.log("redis is now ready"))
-    .on("reconnecting", () => console.log("reconnecting redis"))
-    .on("error", (err) => console.log("Redis Client Error", err))
-    .on("end", () => console.log("redis has stopped"))
-    .connect();
-})();
+client
+  .on("connect", () => console.log("redis connected"))
+  .on("ready", () => console.log("redis is now ready"))
+  .on("reconnecting", () => console.log("reconnecting redis"))
+  .on("error", (err) => console.log("Redis Client Error", err))
+  .on("end", () => console.log("redis has stopped"));
 
 module.exports = client;
